3f06a732d96f50dcc00426e266dadcba964cab51
Faroeq <alves_1044@hotmail.com>
2024-12-28 10:37:03 +0100

    add comments

--- src/hooks/use-spacebar.tsx
+++ src/hooks/use-spacebar.tsx
@@ -1,5 +1,19 @@
 import { useCallback, useEffect } from "react";
 
+/**
+ * Custom hook that triggers a callback function when the spacebar key is pressed. useful for adding any event after pressing the spacebar.
+ *
+ * @param {() => void} callback - The function to be called when the spacebar is pressed.
+ *
+ * @example
+ * ```tsx
+ * useSpacebar(() => {
+ *   console.log("Spacebar pressed!");
+ * });
+ * ```
+ *
+ * @returns {void}
+ */
 function useSpacebar(callback: () => void) {
   const handleSpacebar = useCallback(
     (event: KeyboardEvent) => {
